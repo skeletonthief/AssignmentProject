cbe6179bb5d13567e4191d9e547e295fc8430284
Sarah Pisani <sarah.pisani.e21391@mcast.edu.mt>
2021-01-09 10:25:41 +0100

    Power up spawns randomly

--- Assets/UI Assets/Script/Menu Code/PauseButton.cs
+++ Assets/UI Assets/Script/Menu Code/PauseButton.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class PauseButton : MonoBehaviour
-//{
+public class PauseButton : MonoBehaviour
+{
     // Start is called before the first frame update
     //void Start()
     //{
@@ -14,4 +14,35 @@ using UnityEngine;
     //{
     //    Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f; 
     //}
-//}
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused) 
+            {
+                Resume();
+            }else 
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; 
+        GameIsPaused = false; 
+    }
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; 
+        GameIsPaused = true; 
+    }
+
+}
---
+++ Assets/UI Assets/Script/Power up codes/SpawnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject[] spawnObjects; 
+    //public Transform[] spawnLocations;                //This is turned off since there is only one power up
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(spawnObjects[Random.Range(0, spawnObjects.Length)], this.transform); 
+        //spawnLocations[Random.Range(0, spawnObjects.Length)]);        //This also had to be turned off for now 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
