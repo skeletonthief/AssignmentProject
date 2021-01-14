c202db77152d9bac8623e3cd047b55937518265d
Sarah Pisani <sarah.pisani.e21391@mcast.edu.mt>
2021-01-14 14:30:41 +0100

    Still no luck with platforms
    im close

--- Assets/UI Assets/Script/Platform/PlatformGenetrator.cs
+++ Assets/UI Assets/Script/Platform/PlatformGenetrator.cs
@@ -1,10 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformGenetrator : MonoBehaviour
 {
-    public GameObject thePlatform;
+    [SerializeField]private Transform platformsStart; 
+    [SerializeField] private Transform platforms; 
+    private void Awake()
+    {
+        Transform lastPlatfomsTransform; 
+        //Spawning the platforms by finding the end position
+        lastPlatfomsTransform =SpawnPlatform(platformsStart.Find("EndPosition").position); 
+        lastPlatfomsTransform =SpawnPlatform(lastPlatfomsTransform.Find("EndPosition").position);
+        lastPlatfomsTransform =SpawnPlatform(lastPlatfomsTransform.Find("EndPosition").position);
+        lastPlatfomsTransform =SpawnPlatform(lastPlatfomsTransform.Find("EndPosition").position);
+        
+    }
+    
+    private Transform SpawnPlatform(Vector3 spawnPosition)
+    {
+        Transform platformsTransform = Instantiate(platforms,spawnPosition, Quaternion.identity);
+        return platformsTransform; 
+    }
+    
+    
+    
+    
+    //this is wrong 
+    /*public GameObject thePlatform;
     public Transform generationPoint; 
     public float distanceBetween; 
     private float platformWidth; 
@@ -18,10 +41,12 @@ public class PlatformGenetrator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x > generationPoint.position.x)
+        if(transform.position.x < generationPoint.position.x)
         {
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
-            Instantiate (thePlatform, transform.position, transform.rotation);
+            Instantiate (thePlatform, transform.position, transform.rotation); 
         }
     }
+    */
 }
+
