caf9bd75295dc9c148178da6b58ce5d545c2d2d7
skeletonthief <egdragonmail@gmail.com>
2021-01-15 17:23:22 +0100

    Elevetor Control
    Elevetion in dead spot

---
+++ Assets/Code/environment/Elevator/ElevatorCon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorCon : MonoBehaviour
+{
+    private Vector3 EposA;
+    private Vector3 EposB;
+    private Vector3 Npos;
+
+    [SerializeField]
+    private float MoveSpeed;
+    [SerializeField]
+    private Transform transformB;   
+    [SerializeField]
+    private Transform childTransform; 
+
+    void Start()
+    {
+        EposA = childTransform.localPosition;
+        EposB = transformB.localPosition;
+        Npos = EposB;
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, Npos, MoveSpeed * Time.deltaTime);
+    }
+}
