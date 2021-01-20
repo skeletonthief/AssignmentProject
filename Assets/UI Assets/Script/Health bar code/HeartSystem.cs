1527f3188ce10b0dd2136ea931169d0dc5b85466
Sarah Pisani <sarah.pisani.e21391@mcast.edu.mt>
2021-01-20 12:09:12 +0100

    New platforms added
    also tried working on the heart animation but failed

--- Assets/UI Assets/Script/Health bar code/HeartSystem.cs
+++ Assets/UI Assets/Script/Health bar code/HeartSystem.cs
@@ -8,12 +8,15 @@ public class HeartSystem : MonoBehaviour
     //This is the player's health (player is given 3 lives)
     public GameObject[] hearts; //[0] [1] [2]
     public int life;    //3 
-    //public Animator animator; 
+    //public Transform TriggerAnimation;
+    //private Animation anim; 
     private bool dead; 
-    public Sprite Closedskull; 
+    
     private void Start()
     {
         life = hearts.Length;
+        //anim = gameObject.GetComponent<Animation>();
+        //anim.Stop("CloseHeart");
     }
 
 
@@ -25,39 +28,35 @@ public class HeartSystem : MonoBehaviour
             //Set dead code
             Debug.Log("GAME OVER");
         }
-        //if (life < 1) 
-        //{
-        //   Destroy(hearts[0].gameObject);  
-        //}else if (life < 2)
-        //{
-        //    Destroy(hearts[1].gameObject); 
-        //}else if (life < 3)
-        //{
-        //    Destroy(hearts[2].gameObject); 
-        //}
+        
     }
 
     public void TakeDamage(int d)
     {
-        //Animator anim; 
         if (life >= 1)
         {
             life -= d; //1-1=0
-            Debug.Log("-1 heart");
-            //Destroy(hearts[life].gameObject);  //[0]                                //this is till we add animation
+            //animation when player takes damage                // STIL DOESNT WORK 
+            //anim["CloseHeart"].wrapMode = WrapMode.Once;
+            //anim.Play("CloseHeart");
+            //Debug.Log("-1 heart");
+            
+            //Destroy(hearts[life].gameObject);  //[0]                                
             ////yield return new WaitForSeconds (0.5f);
             //For now just a simple sprite change
-            //this.gameObject.GetComponent<SpriteRenderer>().sprite = Closedskull;
-            
             // Animation when player takes damage instead of being destroyed 
             //anim = hearts.GetComponent<Animator>();                           //I need to fix this 
             //anim.SetBool("Boost", true); 
+            //then to stop animation 
             if(life < 1)
             {
                 dead = true; 
                 SceneManager.LoadScene("Game Over");
             }
         }
-        
+        //else
+        //{
+        //    anim.Stop("CloseHeart");
+        //}
     }
 }
