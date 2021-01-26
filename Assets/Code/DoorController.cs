13fbafd3d1070a4bd030d3d73b737f6db9f468fa
skeletonthief <egdragonmail@gmail.com>
2021-01-26 18:33:50 +0100

    fix
    lava start done
    remove bad scripts

---
+++ Assets/Code/DoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        GameEvents.current.onDoorwayTriggerEnter +=  OnDoorwayOpen;
+    }
+
+    
+    private void OnDoorwayOpen()
+    {
+        LeanTween.moveLocalY(gameObject, 1.6f, 1f).setEaseOutQuad();
+    }
+}
---
+++ Assets/Code/LavaAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaAI : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameEvents.current.onDoorwayTriggerEnter +=  OnDoorwayOpen;
+    }
+
+    private void OnDoorwayOpen()
+    {   
+        //0f (end location), 0f (speed)
+        // speed not lower tham (30f) 
+        LeanTween.moveLocalX(gameObject, -89.8f, 50f).setEaseOutQuad();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+
+    {
+        if(col.gameObject.CompareTag("Player")) //If a player hits the collider of this obstacle
+        {
+                Destroy(col.gameObject);
+        }
+        if(col.gameObject.CompareTag("Enemy")) //If a Enemy hits the collider of this obstacle
+        {
+                Destroy(col.gameObject);
+        }
+    }
+
+}
+
